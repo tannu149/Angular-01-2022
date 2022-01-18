53f425d9293ce45a8451100d8eed650992741eed
tannu149 <unttannu@gmail.com>
2022-01-18 11:12:53 +0100

    Uued komponendid routingud ja liikumine

--- veebipood/src/app/app-routing.module.ts
+++ veebipood/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvalehtComponent } from './avaleht/avaleht.component';
+import { OstukorvComponent } from './ostukorv/ostukorv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component:AvalehtComponent },
+  { path: "ostukorv", component:OstukorvComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
